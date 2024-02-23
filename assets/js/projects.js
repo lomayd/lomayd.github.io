da8ddfdac5e3adf8ac7c5a2c2d2a98a0a938392e
lomayd <jinho10167@gmail.com>
2024-02-23 16:08:33 +0900

    refactor: projects.html

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,148 +11,157 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
-            link: 'https://github.com/abhn/Mporter',
-            title: 'Mporter',
-            demo: 'https://mporter.co',
-            technologies: ['Flask', 'Celery', 'Python'],
-            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
+            image: 'assets/images/case.png',
+            link: 'https://github.com/khu-capstone-design-case/case-backend',
+            title: 'CASE',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL', 'MongoDB'],
+            description: "'CASE' is a call recording summary and emotion (nuance) analysis web service",
             categories: ['featured', 'webdev']
         },
         {
-            image: 'assets/images/mobile-landscape.jpg',
-            link: 'https://github.com/abhn/Wall-E',
-            title: 'Wall-E',
-            demo: 'http://wall-e-jekyll.github.io/',
-            technologies: ['Semantic UI', 'Jekyll'],
-            description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
+            image: 'assets/images/mysql-replication-delay.png',
+            link: 'https://github.com/lomayd/khu-graduate-project',
+            title: 'MySQL Replication Delay',
+            demo: false,
+            technologies: ['MySQL'],
+            description: "Study on Causes and Solutions for MySQL Replication Delay Issues",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/parenting-sim.png',
+            link: 'https://github.com/khuthon-parenting-sim/parenting-sim-backend',
+            title: '금시초문(Parenting-Sim)',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL'],
+            description: "'금시초문' is parenting information service for new parents",
             categories: ['featured', 'webdev']
         },
         {
-            image: 'assets/images/collage.jpg',
-            link: 'https://github.com/abhn/Marvel',
-            title: 'Marvel',
+            image: 'assets/images/dbms-replication.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-Replication',
+            title: 'DBMS-Replication',
             demo: false,
-            technologies: ['Android', 'OpenCV'],
-            description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
-            categories: ['featured', 'native']
+            technologies: ['MySQL'],
+            description: "MySQL Master-Slave DB Replication",
+            categories: ['lab']
         },
         {
-            image: 'assets/images/mpw.jpg',
-            link: 'https://github.com/abhn/mpw',
-            title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
-            technologies: ['Semantic UI', 'CSS3'],
-            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-            categories: ['featured', 'security']
-        },
-        {
-            image: 'assets/images/social-share-count.jpeg',
-            link: 'https://github.com/abhn/Social-Share-Counts',
-            title: 'Social Share Count',
+            image: 'assets/images/dbms-optimizing.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-Optimizing',
+            title: 'DBMS-Optimizing',
             demo: false,
-            technologies: ['Python'],
-            description: "Ever wondered how many times a URL has been shared on popular social networks?",
-            categories: ['native']
+            technologies: ['MySQL'],
+            description: "Reducing DBMS Execution Time",
+            categories: ['lab']
         },
         {
-            image: 'assets/images/data-destroyer.png',
-            link: 'https://github.com/abhn/data-destroyer-gui',
-            title: 'Data Destroyer',
+            image: 'assets/images/dbms-logging.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-Logging',
+            title: 'DBMS-Logging',
             demo: false,
-            technologies: ['C++', 'Qt'],
-            description: "Native GUI wrapper for GNU coreutils tool 'dd'",
-            categories: ['native']
+            technologies: ['ElasticSearch', 'Kibana'],
+            description: "Request / Response Logging Using ELK",
+            categories: ['lab']
         },
         {
-            image: 'assets/images/raspberry-pi-monitor.png',
-            link: 'https://github.com/abhn/RPi-Status-Monitor',
-            title: 'Raspberry Pi Monitor',
+            image: 'assets/images/spring-graphql.png',
+            link: 'https://github.com/lomayd/Spring-Lab-GraphQL',
+            title: 'Spring-GraphQL',
             demo: false,
-            technologies: ['python', 'flask'],
-            description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
-            categories: ['webdev', 'diy']
+            technologies: ['SpringBoot'],
+            description: "Spring For GraphQL",
+            categories: ['lab']
         },
         {
-            image: 'assets/images/s3scan.png',
-            link: 'https://github.com/abhn/S3Scan',
-            title: 'S3Scan',
+            image: 'assets/images/dbms-querydsl.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-QueryDSL',
+            title: 'DBMS-QueryDSL',
             demo: false,
-            technologies: ['python'],
-            description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
-            categories: ['native', 'security']
+            technologies: ['MySQL'],
+            description: "QueryDSL JPA",
+            categories: ['lab']
         },
         {
-            image: 'assets/images/elementary.png',
-            link: 'https://github.com/abhn/Elementary',
-            title: 'Elementary',
-            demo: 'https://elementary-jekyll.github.io/',
-            technologies: ['Jekyll', 'CSS3'],
-            description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
+            image: 'assets/images/dbms-mybatis.jpeg',
+            link: 'https://github.com/lomayd/DBMS-Lab-Mybatis',
+            title: 'DBMS-Mybatis',
+            demo: false,
+            technologies: ['MySQL'],
+            description: "Mybatis",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/dbms-sharding.jpeg',
+            link: 'https://github.com/lomayd/DBMS-Lab-Sharding',
+            title: 'DBMS-Sharding',
+            demo: false,
+            technologies: ['MySQL'],
+            description: "Sharded MySQL Cluster Using Apache ShardingSphere",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/dbms-caching.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-Caching',
+            title: 'DBMS-Caching',
+            demo: false,
+            technologies: ['Redis'],
+            description: "Cache Server Using Redis",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/nearbuy.png',
+            link: 'https://github.com/Dcom-KHU/nearbuy-backend',
+            title: 'NearBuy',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL', 'MongoDB'],
+            description: "'NearBuy' is a neighborhood transaction platform for single-person households and students living alone",
+            categories: ['featured', 'webdev']
+        },
+        {
+            image: 'assets/images/yoursampple.png',
+            link: 'https://github.com/lomayd/YourSampple-Remake',
+            title: 'YourSampple',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL'],
+            description: "'YourSampple' is a phone bill calculator",
             categories: ['webdev']
         },
         {
-            image: 'assets/images/soot-spirits.png',
-            link: 'https://github.com/abhn/Soot-Spirits',
-            title: 'Soot Spirits',
-            demo: 'https://sootspirits.github.io',
-            technologies: ['Jekyll', 'CSS3'],
-            description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
+            image: 'assets/images/dbms-nosql.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-NoSQL',
+            title: 'DBMS-NoSQL',
+            demo: false,
+            technologies: ['Redis', 'MongoDB', 'ElasticSearch'],
+            description: "NoSQL CRUD",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/dbms-rdbms.png',
+            link: 'https://github.com/lomayd/DBMS-Lab-RDBMS',
+            title: 'DBMS-RDBMS',
+            demo: false,
+            technologies: ['Oracle', 'MySQL', 'MSSQL', 'PostgreSQL'],
+            description: "RDBMS CRUD",
+            categories: ['lab']
+        },
+        {
+            image: 'assets/images/ssd.png',
+            link: 'https://github.com/new-tech-project-2/ssd-backend',
+            title: 'SSD',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL'],
+            description: "'SSD' is short for Smart Soju Dispenser, and is a smart soju dispenser using NFC",
+            categories: ['featured', 'webdev']
+        },
+        {
+            image: 'assets/images/refrigerator.png',
+            link: 'https://github.com/Dcom-KHU/refrigerator-api-server',
+            title: '냉장고를부탁해(Refrigerator)',
+            demo: false,
+            technologies: ['SpringBoot', 'MySQL'],
+            description: "'냉장고를 부탁해' is a service that helps you manage the ingredients in your refrigerator and recommend food recipes tailored to the ingredients you have",
             categories: ['webdev']
-        },
-        {
-            image: 'assets/images/python-chat.png',
-            link: 'https://www.nagekar.com/2014/12/lan-group-messenger-in-python.html',
-            title: 'Terminal Group Chat',
-            demo: false,
-            technologies: ['Python', 'Sockets'],
-            description: "Simple terminal group chat based on native sockets using Python.",
-            categories: ['native']
-        },
-        {
-            image: 'assets/images/old-lcd.jpg',
-            link: 'https://www.nagekar.com/2018/05/reusing-old-laptop-lcd-panel.html',
-            title: 'Reusing Old LCD Panel',
-            demo: false,
-            technologies: ['DIY'],
-            description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/nextcloud-enc.png',
-            link: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Encrypted Self-Hosted Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'GnuPG'],
-            description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/google-cloud-backup.png',
-            link: 'https://www.nagekar.com/2018/05/encrypted-backup-with-duplicity.html',
-            title: 'Encrypted Backups - Google Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'Duplicity'],
-            description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/pi-cloud.jpg',
-            link: 'https://www.nagekar.com/2016/01/how-to-private-local-cloud-using-raspberrypi.html',
-            title: 'Local Cloud - Raspberry Pi',
-            demo: false,
-            technologies: ['FTP', 'DIY'],
-            description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/koalamate.png',
-            link: 'https://github.com/abhn/koalamate',
-            title: 'Koalamate',
-            demo: false,
-            technologies: ['Electron', 'Javascript'],
-            description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
-            categories: ['native']
         },
     ]
 
